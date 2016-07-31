55c273da3b9f1e2d3af27984f3bc4bd89b31ee3f
MargaritaSv <margarita.esti@gmail.com>
2016-07-31 09:29:03 +0300

    added more necessary fields in class Shop

--- Shop.java
+++ Shop.java
@@ -1,10 +1,19 @@
 package shopBayers;
 
+import java.util.ArrayList;
+
 /**
  * Created by Magi on 31.7.2016 г..
  */
 public class Shop {
     private String name;
     private String address;
+    private double money;
+    private ArrayList<Product> products;
 
+    public Shop(String name, String address, double money) {
+        this.name = name;
+        this.address = address;
+        this.money = money;
+    }
 }
